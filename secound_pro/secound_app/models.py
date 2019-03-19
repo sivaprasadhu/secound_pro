cafe1844c99e2af01ed60b8dcc33281210555baf
sivareddy <sivareddy4255@gmail.com>
2019-03-19 23:30:17 +0530

    Model_commit

--- secound_pro/secound_app/models.py
+++ secound_pro/secound_app/models.py
@@ -1,3 +1,7 @@
 from django.db import models
 
 # Create your models here.
+class Employee(models.Model):
+    name=models.CharField(max_length=100)
+    salary=models.BigIntegerField()
+    address=models.CharField(max_length=20)
